1ee3bb1777180cea205c7716d567f1bb90088e83
Ganesh Bhat <ganeshbhat@MacBook-Pro.local>
2025-04-06 12:03:16 +0530

    ver 1.1.0

--- index.php
+++ index.php
@@ -1,28 +1,30 @@
-
-
-
 <!DOCTYPE html>
+<?php include 'header.php';
+$db = new SQLite3("database/database.sqlite");
+?>
 <html lang="en" >
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Travel Adventures</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-</head>
-<body class="bg-gradient-to-br overflow-y-scroll h-screen max-w-7xl mx-auto from-blue-50 to-blue-400">
-<?php include 'header.php';?>
+<body class="bg-gradient-to-br overflow-y-scroll min-h-screen max-w-7xl mx-auto from-blue-50 to-blue-400">
+
 <section class="text-gray-600 body-font">
-    <div class="container px-5 pt-24 pb-4 mx-auto">
-        <div class="grid lg:grid-cols-3 -m-4">
-            <div class="p-4 ">
+    <div class="container flex flex-col px-5 mt-8 pb-4 mx-auto">
+        <p class="-ml-4 my-4">Travel around the best islands in the world hassle free with us. We offer various services from booking flights,hotels and visa services. All our island tours come with full boarded meals and tour guides. </p>
+        <div class="flex flex-col -ml-4 lg:w-1/3">
+            <input name="search" type="text" class="border-2 border-gray-200 p-2 rounded-lg" placeholder="Search Places"/>
+        </div>
+
+        <div class="grid lg:grid-cols-3 mt-4 gap-4 -m-4">
+            <?php $result = $db->query("select * from places");
+            while ($row = $result->fetchArray()) {
+            ?>
+            <div class=" ">
                 <div class="h-full bg-gray-50 border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="assets/bali.jpeg" alt="travel">
+                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="<?php echo $row['image_url'] ?>" alt="travel">
                     <div class="p-6">
-                        <h2 class="text-base font-medium text-indigo-300 mb-1">Discover Indonesia</h2>
-                        <h1 class="text-2xl font-semibold mb-3">Bali</h1>
-                        <p class="leading-relaxed mb-3">A vibrant Indonesian island known for its forested volcanic mountains, iconic rice paddies, beaches, and coral reefs.</p>
+                        <h2 class="text-base font-medium text-indigo-300 mb-1"><?php echo $row['short_description'] ?></h2>
+                        <h1 class="text-2xl font-semibold mb-3"><?php echo $row['name'] ?></h1>
+                        <p class="leading-relaxed mb-3"><?php echo $row['description'] ?></p>
                         <div class="flex items-center flex-wrap ">
-                            <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                            <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Reviews
                                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                     <path d="M5 12h14"></path>
                                     <path d="M12 5l7 7-7 7"></path>
@@ -30,63 +32,26 @@
                             </a>
                             <span class="text-gray-400 inline-flex items-center leading-none text-sm">
                                     <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                    30k visitors/month
+                                    <?php echo $row['visitors'] ?> visitors/month
                                 </span>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class="p-4 ">
-                <div class="h-full bg-gray-50 border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="assets/seychelles.jpeg" alt="travel">
-                    <div class="p-6">
-                        <h2 class="text-base font-medium text-indigo-300 mb-1">Discover Africa</h2>
-                        <h1 class="text-2xl font-semibold mb-3">Seychelles</h1>
-                        <p class="leading-relaxed mb-3"> An archipelago of 115 islands in the Indian Ocean, renowned for its pristine beaches, crystal-clear waters, and diverse marine life.</p>
-                        <div class="flex items-center flex-wrap ">
-                            <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path d="M5 12h14"></path>
-                                    <path d="M12 5l7 7-7 7"></path>
-                                </svg>
-                            </a>
-                            <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                                    <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                    50k visitors/month
-                            </span>
+                        <div class="mt-5 inline-flex lg:justify-end">
+                            <form method="POST" action="user/bookings.php">
+                                <button type="submit" class="inline-flex text-gray-200 items-center bg-blue-600 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 hover:text-base rounded text-base mt-4 md:mt-0">Book Now
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="p-4 ">
-                <div class="h-full bg-gray-50 border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="assets/bora_bora.jpeg" alt="travel">
-                    <div class="p-6">
-                        <h2 class="text-base font-medium text-indigo-300 mb-1">Discover Polynesia</h2>
-                        <h1 class="text-2xl font-semibold mb-3">Bora Bora</h1>
-                        <p class="leading-relaxed mb-3"> A small South Pacific island northwest of Tahiti in French Polynesia, famous for its turquoise lagoon, luxury resorts, and scuba diving.</p>
-                        <div class="flex items-center flex-wrap ">
-                            <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path d="M5 12h14"></path>
-                                    <path d="M12 5l7 7-7 7"></path>
-                                </svg>
-                            </a>
-                            <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                                    <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                    60k visitors/month
-                                </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <?php } ?>
         </div>
     </div>
     <div>
         <div class="container mx-auto px-6">
             <div class="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
                 <div class="sm:w-2/3 text-center py-6">
-                    <p class="text-sm text-gray-700 font-bold mb-2">© 2021 by Travel Adventures</p>
+                    <p class="text-sm text-gray-700 font-bold mb-2">© <?php echo date("Y") ?> by Travel Adventures</p>
                 </div>
             </div>
         </div>
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Vulnerable Code
+The aim of this code is to demonstrate a simple example of a vulnerable code that is susceptible to SQL injection attacks.
+
+This is a simple PHP script that connects to a MySQL database and retrieves user information based on a username provided via a GET request. The code is vulnerable to SQL injection because it directly incorporates user input into the SQL query without proper sanitization or parameterization.
+
